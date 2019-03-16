ed4f7f09c7f47ce54f02527445100aa014ae4d1e
david bernier <davidbernier91@gmail.com>
2019-03-16 10:46:50 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,22 @@
 class Dog
 
-  def initialize(name, breed="Mutt")
-    @name = name
-    @breed = breed
+	attr_accessor :name, :breed
 
-  end
+	def initialize(name, breed = "Mutt")
+		@name = name # ==> instance variable
+		@breed = breed # ==>instance variable
+	end
+
 
 end
+
+
+dog1 = Dog.new("fido")
+
+puts dog1.name
+
+# Name is derived from arg
+# Breed is derived from arg with a default of "Mutt". 
+
+# Can a read/write name and breed  
+#
